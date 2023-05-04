f919b7ed96f55fd398791d29c62ab4997826b644
Botirjon0708 <botirjonteshaboyev0708@gmail.com>
2023-05-04 12:56:18 +0300

    used conditions, operators and loop

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 const numberOfFilms = +prompt("How many movies have you already watched? ", "");
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
 
-const a = prompt("one of the last movies you watched?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("one of the last movies you watched?", ""),
-    d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[ a ] = b
-personalMovieDB.movies[ c ] = d
+for (let i = 0; i < 2; i++){
+  const a = prompt("One of the last movies have you watched?", ""),
+    b = prompt("На сколько оцените его?", "");
+  
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    
+    personalMovieDB.movies[ a ] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+  
+}
+if (personalMovieDB.count < 10) {
+    console.log("Watched quite a less movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
+    console.log("You are a good  viewer");
+} else if (personalMovieDB.count >= 30) {
+  console.log("You are a cinemaddict");
+
+} else {
+  console.log("Error");
+}
+  
 
 console.log(personalMovieDB);
+
 
