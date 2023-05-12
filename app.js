9ee264dd3da60035eff0662407722d5fbb708a78
Botirjon0708 <botirjonteshaboyev0708@gmail.com>
2023-05-12 09:36:16 +0300

    Done with function

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt("How many movies have you already watched? ", "");
+let numberOfFilms;
+
+function start() {
+   numberOfFilms = +prompt("How many movies have you already watched? ", "");
+  
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN (numberOfFilms)){
+    numberOfFilms = +prompt("How many movies have you already watched? ", "");
+  }
+}
+
+start()
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,34 +18,51 @@ const personalMovieDB = {
   privat: false,
 };
 
-
-
-for (let i = 0; i < 2; i++){
-  const a = prompt("One of the last movies have you watched?", ""),
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("One of the last movies have you watched?", ""),
     b = prompt("На сколько оцените его?", "");
-  
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
     
-    personalMovieDB.movies[ a ] = b;
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
   }
-  
 }
-if (personalMovieDB.count < 10) {
-    console.log("Watched quite a less movies");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
-    console.log("You are a good  viewer");
-} else if (personalMovieDB.count >= 30) {
-  console.log("You are a cinemaddict");
 
-} else {
-  console.log("Error");
+rememberMyFilms(); 
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Watched quite a less movies");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a good  viewer");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("You are a cinemaddict");
+  } else {
+    console.log("Error");
+  }
 }
-  
+detectPersonalLevel();
 
 console.log(personalMovieDB);
 
 
+function showMyDb(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+showMyDb(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++){
+    const genre = prompt(`Your favorite genre ${i}`);
+    personalMovieDB.genres[ i - 1 ] = genre;
+  }
+}
+
+writeYourGenres();
